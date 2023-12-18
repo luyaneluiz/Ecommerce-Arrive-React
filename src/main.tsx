758848b06bb8e95a5ce7bf4b8c64f9cf323ff80d
luyaneluiz <luyane.luiz.ll@gmail.com>
2023-12-18 14:09:42 -0300

    feat: instalação do mantine

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,17 @@ import { router } from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 )
