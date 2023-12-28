d719b495a241eae4a41969a518b52b32c328bb98
luyaneluiz <luyane.luiz.ll@gmail.com>
2023-12-28 12:11:33 -0300

    feat: criando interface de products

---
+++ src/components/ProductTypes.tsx
@@ -0,0 +1,10 @@
+export interface ProductProps {
+  id: number;
+  title: string;
+  category: string;
+  price: number;
+  old_price: number;
+  cover: string;
+  description: string;
+  type: string;
+}
