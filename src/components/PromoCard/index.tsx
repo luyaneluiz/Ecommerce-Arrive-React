dfd9385f2b3a73434371a9b13c6e859b94e52218
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-03 16:40:08 -0300

    fix: ajustando larguras dos itens do card promo no desktop

--- src/components/PromoCard/index.tsx
+++ src/components/PromoCard/index.tsx
@@ -83,57 +83,60 @@ export function PromoCard() {
 
   if (product) {
     return (
-      <div className="flex flex-col items-center px-7">
+      <div className="flex flex-col items-center sm:ps-7">
         <div className="w-full border-b mb-4 py-3">
-          <h3 className="font-bold">Daily Deal</h3>
+          <h3 className="font-bold sm:text-lg">Daily Deal</h3>
         </div>
 
-        <div className="flex flex-col border rounded-xl p-6">
+        <div className="flex flex-col border rounded-xl p-6 w-full sm:flex-row">
           <img
             src={product.cover}
             alt={product.title}
-            className="w-full p-3 cursor-pointer transition-all hover:scale-105"
+            className="w-full sm:max-w-[400px] px-3 cursor-pointer transition-all hover:scale-105"
           />
 
-          <div className="flex flex-col">
-            <h4 className="overflow-hidden whitespace-nowrap text-ellipsis font-bold mb-1">
-              {product.title}
-            </h4>
-            <p className="text-xs">{product.description}</p>
+          <section className="sm:ps-3">
+            <div className="flex flex-col">
+              <h4 className="overflow-hidden whitespace-nowrap text-ellipsis font-bold mb-1 sm:text-2xl">
+                {product.title}
+              </h4>
 
-            <div className="flex items-center gap-1 my-2">
-              <h3 className="text-pink font-bold text-lg">
-                ${product.price.toFixed(2)}
-              </h3>
-              <del className="text-sm">${product.old_price.toFixed(2)}</del>
-            </div>
+              <p className="text-xs">{product.description}</p>
 
-            <button className="bg-pink font-bold text-white cursor-pointer rounded-lg py-2 transition-all hover:bg-lightPink">
-              SHOP NOW
-            </button>
-
-            <div>
-              <div className="flex justify-between text-xs my-3">
-                <p>
-                  SOLD: <b>30</b>
-                </p>
-                <p>
-                  AVALIABE: <b>20</b>
-                </p>
+              <div className="flex items-center gap-1 my-2">
+                <h3 className="text-pink font-bold text-lg sm:text-2xl">
+                  ${product.price.toFixed(2)}
+                </h3>
+                <del className="text-sm">${product.old_price.toFixed(2)}</del>
               </div>
-              <Progress
-                value={60}
-                placeholder=""
-                size="lg"
-                className="border bg-gray-900/5 p-1"
-                color="purple"
-              />
-            </div>
-          </div>
 
-          <section className="mt-4">
-            <h5 className="font-bold uppercase">Offer ends in</h5>
-            <Timer {...timerValues} />
+              <button className="bg-pink font-bold text-white cursor-pointer rounded-lg py-2 transition-all hover:bg-lightPink sm:w-32 sm:text-sm sm:my-2">
+                SHOP NOW
+              </button>
+
+              <div>
+                <div className="flex justify-between text-xs my-3">
+                  <p>
+                    SOLD: <b>30</b>
+                  </p>
+                  <p>
+                    AVALIABE: <b>20</b>
+                  </p>
+                </div>
+                <Progress
+                  value={60}
+                  placeholder=""
+                  size="lg"
+                  className="border bg-gray-900/5 p-1"
+                  color="purple"
+                />
+              </div>
+            </div>
+
+            <section className="mt-4">
+              <h5 className="font-bold uppercase">Offer ends in</h5>
+              <Timer {...timerValues} />
+            </section>
           </section>
         </div>
       </div>
