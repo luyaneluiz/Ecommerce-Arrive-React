a77e46d83253f028d8862c33fae622f1298e015f
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-03 17:50:29 -0300

    fix: ajustando uso dos dados recebidos no parametro

--- src/components/Trends/ProductCard.tsx
+++ src/components/Trends/ProductCard.tsx
@@ -2,21 +2,19 @@
 import { ProductProps } from "../ProductTypes";
 
 export function ProductCard({ product }: { product: ProductProps }) {
-  const { title, category, price, old_price, cover } = product;
-
   return (
     <div className="flex gap-4 w-full p-4 rounded-2xl mt-4 shadow-lg">
       <div className="flex justify-center h-20 w-20 overflow-hidden">
-        <img src={cover} alt={title} className="h-20" />
+        <img src={product.cover} alt={product.title} className="h-20" />
       </div>
       <div className="w-4/5">
         <h4 className="text-ellipsis overflow-hidden whitespace-nowrap">
-          {title}
+          {product.title}
         </h4>
-        <p>{category}</p>
+        <p>{product.category}</p>
         <div className="flex items-center">
-          <h3 className="text-pink pe-2">${price.toFixed(2)}</h3>
-          <del className="text-xs">${old_price.toFixed(2)}</del>
+          <h3 className="text-pink pe-2">${product.price.toFixed(2)}</h3>
+          <del className="text-xs">${product.old_price.toFixed(2)}</del>
         </div>
       </div>
     </div>
