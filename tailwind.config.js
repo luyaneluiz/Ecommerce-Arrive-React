52b4a705efbc1c4e73deb574390f616bd67432ff
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-03 16:06:17 -0300

    feat: instalando material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         brown: "#44312b",
-        offWhite: "#f2f2f2", 
+        offWhite: "#f2f2f2",
         pink: "#c45389",
-        lightPink: "#d369a0"
+        lightPink: "#d369a0",
       },
       padding: {
-        '8%': '8%',
-      }
+        "8%": "8%",
+      },
     },
   },
   plugins: [],
-}
-
+});
