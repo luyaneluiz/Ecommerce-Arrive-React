95b28c40a7bc01b248459340c65ac596989d5a68
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-04 23:13:16 -0300

    style: adicionando espaçamento abaixo dos products

--- src/pages/Home/Products/index.tsx
+++ src/pages/Home/Products/index.tsx
@@ -17,7 +17,7 @@ export function Products() {
   }, []);
 
   return (
-    <section className="flex flex-col items-center sm:ps-7">
+    <section className="flex flex-col items-center pb-6 sm:ps-7">
       <div className="w-full border-b mb-4 py-3">
         <h3 className="font-bold sm:text-lg">Products</h3>
       </div>
