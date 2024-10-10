792c499910badf58361847117403dc711a99e082
luyane.luiz <luyane.luiz.ll@gmail.com>
2024-10-10 17:36:11 -0300

    criando estrutura basica do backend em node

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import products from './routes/products';
+
+const app = express();
+const port = 3002;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.use('/api', products);
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+});
