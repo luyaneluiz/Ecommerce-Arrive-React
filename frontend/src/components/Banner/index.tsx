4f1cb47a7926fe782c152bd54424df012cd9b094
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-10-12 18:31:41 -0300

    Adicionando autoplay no carrossel

--- frontend/src/components/Banner/index.tsx
+++ frontend/src/components/Banner/index.tsx
@@ -2,13 +2,23 @@ import React from "react"
 import { Carousel } from "@mantine/carousel"
 import { useBreakpoint } from "../../contexts/BreakpointContext"
 import { banners } from "../../assets/banner/banners"
+import { useRef } from "react"
+import Autoplay from "embla-carousel-autoplay"
 
 export function Banner() {
     const { isMobile } = useBreakpoint()
+    const autoplay = useRef(Autoplay({ delay: 2000 }))
 
     return (
         <div className="w-full">
-            <Carousel withIndicators loop slideSize="100%">
+            <Carousel
+                withIndicators
+                loop
+                slideSize="100%"
+                plugins={[autoplay.current]}
+                onMouseEnter={autoplay.current.stop}
+                onMouseLeave={autoplay.current.reset}
+            >
                 {banners.map((banner, index) => (
                     <Carousel.Slide key={index} className="w-full">
                         <img
