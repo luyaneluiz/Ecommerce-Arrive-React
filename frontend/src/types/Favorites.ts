e53e5d61291614af03259fac488113e2dc148e7b
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-12-20 08:27:49 -0300

    adicionando setFavorites ao tipo FavoritesProps

--- frontend/src/types/Favorites.ts
+++ frontend/src/types/Favorites.ts
@@ -2,6 +2,7 @@ import { ProductProps } from "./ProductTypes"
 
 export interface FavoritesProps {
     favorites: ProductProps[] | null
+    setFavorites: React.Dispatch<React.SetStateAction<ProductProps[] | null>>
     loading: boolean
     error: string | null
 }
