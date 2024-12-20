0fd831eb7b31d41102cff74fa10af35225bcef76
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-12-20 09:35:00 -0300

    refatorando rotas para separar AuthRoutes e Routes no arquivo App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,7 @@
-import React from "react"
+import { Routes } from "./routes/routes"
 import { createBrowserRouter } from "react-router-dom"
+import { AuthRoutes } from "./routes/auth"
 
-import { Layout } from "./components/Layout"
-
-import { Home } from "./pages/Home"
-import { Profile } from "./pages/Profile"
-import { Favorites } from "./pages/Favorites"
-import { Bag } from "./pages/Bag"
-import { New } from "./pages/New"
-import { Trends } from "./pages/Trends"
-import { Offers } from "./pages/Offers"
-import { Product } from "./pages/Product"
-import CreateProduct from "./pages/Admin/CreateProduct"
-
-const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/profile",
-                element: <Profile />,
-            },
-            {
-                path: "/favorites",
-                element: <Favorites />,
-            },
-            {
-                path: "/bag",
-                element: <Bag />,
-            },
-            {
-                path: "/new",
-                element: <New />,
-            },
-            {
-                path: "/trends",
-                element: <Trends />,
-            },
-            {
-                path: "/offers",
-                element: <Offers />,
-            },
-            {
-                path: "/create/product",
-                element: <CreateProduct />,
-            },
-            {
-                path: "/product/:id",
-                element: <Product />,
-            },
-        ],
-    },
-])
+const router = createBrowserRouter([...AuthRoutes, ...Routes])
 
 export { router }
---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Layout } from "../components/Layout"
+import { Trends } from "../components/Trends"
+import { Bag } from "../pages/Bag"
+import { Home } from "../pages/Home"
+import { New } from "../pages/New"
+import { Offers } from "../pages/Offers"
+import { Product } from "../pages/Product"
+
+export const Routes = [
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "/bag",
+                element: <Bag />,
+            },
+            {
+                path: "/new",
+                element: <New />,
+            },
+            {
+                path: "/trends",
+                element: <Trends />,
+            },
+            {
+                path: "/offers",
+                element: <Offers />,
+            },
+            {
+                path: "/product/:id",
+                element: <Product />,
+            },
+        ],
+    },
+]
