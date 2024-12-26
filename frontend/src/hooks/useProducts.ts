407dbf9d44301628223d893f5fd75812f95a9101
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-12-26 10:43:25 -0300

    atualizando importações para caminhos absolutos e tipando o estado de erro no hook useProducts

--- frontend/src/hooks/useProducts.ts
+++ frontend/src/hooks/useProducts.ts
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react"
-import { api } from "../services/api"
-import { ProductProps } from "../types/ProductTypes"
+import { api } from "@/services/api"
+import { ProductProps } from "@/types/ProductTypes"
 
 export const useProducts = () => {
     const [products, setProducts] = useState<ProductProps[]>([])
-    const [error, setError] = useState(null)
+    const [error, setError] = useState<unknown>(null)
 
     useEffect(() => {
         const fetchProducts = async () => {
             try {
                 const response = await api.get("/products")
+
                 setProducts(response.data)
             } catch (error) {
                 setError(error)
