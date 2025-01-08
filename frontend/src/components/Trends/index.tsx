eea2fe6f544f2390d459421eef64a014d83bbf19
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-01-08 11:03:03 -0300

    refactor: remover componentes DesktopTrends e MobileTrends; adicionar HeaderTrend para melhor gerenciamento de tendências

--- frontend/src/components/Trends/index.tsx
+++ frontend/src/components/Trends/index.tsx
@@ -1,33 +1,60 @@
-import React, { useState } from "react"
+import { useMemo, useState } from "react"
 import { useProducts } from "../../hooks/useProducts"
 import { useBreakpoint } from "../../contexts/BreakpointContext"
-import MobileTrends from "./Mobile"
-import DesktopTrends from "./Desktop"
+import { Stack } from "@mantine/core"
+import HeaderTrend from "./Header"
+import { TrendProductCard } from "./Card"
+
+type Trend = "New" | "Hot"
 
 export function Trends() {
     const { isMobile } = useBreakpoint()
     const { products } = useProducts()
-    const [trend, setTrend] = useState<"New" | "Hot">("New")
+    const [trend, setTrend] = useState<Trend>("New")
 
     const filteredProducts = products.filter(
-        (product) => product.type === trend || !isMobile,
+        (product) => product.type === trend,
     )
 
-    const handleTrendClick = () => {
-        setTrend(trend === "New" ? "Hot" : "New")
-    }
+    const trendsData = useMemo(
+        () => ({
+            New: products.filter((product) => product.type === "New"),
+            Hot: products.filter((product) => product.type === "Hot"),
+        }),
+        [products],
+    )
+
+    const handleChangeTrend = () => setTrend(trend === "New" ? "Hot" : "New")
 
     return (
-        <div className="flex justify-center mb-6 w-full lg:max-w-[370px]">
+        <section className="mb-6 w-full lg:max-w-[370px]">
             {isMobile ? (
-                <MobileTrends
-                    products={filteredProducts}
-                    trend={trend}
-                    handleTrendClick={handleTrendClick}
-                />
+                <>
+                    <HeaderTrend
+                        trend={trend}
+                        changeTrend={handleChangeTrend}
+                        hasArrows={isMobile}
+                    />
+
+                    {filteredProducts.map((product) => (
+                        <TrendProductCard key={product._id} product={product} />
+                    ))}
+                </>
             ) : (
-                <DesktopTrends products={products} />
+                <Stack>
+                    {Object.entries(trendsData).map(([key, products]) => (
+                        <Stack key={key}>
+                            <HeaderTrend trend={key as Trend} />
+                            {products.map((product) => (
+                                <TrendProductCard
+                                    key={product._id}
+                                    product={product}
+                                />
+                            ))}
+                        </Stack>
+                    ))}
+                </Stack>
             )}
-        </div>
+        </section>
     )
 }
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -9,10 +9,9 @@ export function Home() {
         <div className="flex flex-col items-center w-full">
             <Banner />
             <CategoriesCards />
-            <div className="flex flex-col lg:flex-row lg:gap-4 w-full px-6 items-start">
-                <div className="flex justify-center w-full lg:max-w-[370px]">
-                    <Trends />
-                </div>
+
+            <div className="flex flex-col lg:flex-row lg:gap-4 w-full px-8 items-start">
+                <Trends />
                 <div className="w-full flex flex-col gap-4">
                     <PromoCard />
                     <Products />
