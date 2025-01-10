f0155b16843fc7688a25113316c9a23dc21e0ae5
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-01-10 08:21:14 -0300

    refactor: remover margem inferior do componente Title para melhor alinhamento

--- frontend/src/components/Titlte/index.tsx
+++ frontend/src/components/Titlte/index.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
 
 export function Title({ text }: TitleProps) {
     return (
-        <div className="w-full border-b mb-4 py-3">
+        <div className="w-full border-b py-3">
             <h3 className="font-bold sm:text-lg">{text}</h3>
         </div>
     )
