57b964e09a8532215e65eec4d09e3508cabc53d3
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-01-20 15:13:40 -0300

    refactor: atualizar o componente Favorites para usar Stack e ajustar id do usuario conforme armazenado em localStorage no login

--- frontend/src/pages/Favorites/index.tsx
+++ frontend/src/pages/Favorites/index.tsx
@@ -1,26 +1,25 @@
-import { SimpleGrid, Title } from "@mantine/core"
+import { SimpleGrid, Stack } from "@mantine/core"
 import FavoriteCard from "../../components/FavoriteCard"
 import { useFavorites } from "../../hooks/useFavorites"
 import EmptyPage from "@/components/Empty"
+import { Title } from "@/components/Titlte"
 
 export function Favorites() {
-    const userId = "6718fc3fe2238b22cef334d4"
+    const userId = localStorage.getItem("user")
     const { favorites } = useFavorites(userId)
 
     return (
-        <main className="px-8">
+        <main className="px-4 md:px-12 pb-8">
             {favorites && favorites.length > 0 ? (
-                <>
-                    <Title order={1} size={24} pb={20}>
-                        Favorites
-                    </Title>
+                <Stack>
+                    <Title text="Favorites" />
 
-                    <SimpleGrid cols={{ base: 1, sm: 3, md: 4, lg: 5 }}>
+                    <SimpleGrid cols={{ base: 2, sm: 3, md: 4, lg: 5 }}>
                         {favorites.map((product) => (
                             <FavoriteCard key={product._id} product={product} />
                         ))}
                     </SimpleGrid>
-                </>
+                </Stack>
             ) : (
                 <EmptyPage />
             )}
