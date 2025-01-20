8daf3a27673391720d9c2b99e73475b4992170bb
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-01-20 16:40:50 -0300

    feat: refatorar componente Cart para melhorar a exibição de produtos e estado vazio

--- frontend/src/pages/Cart/index.tsx
+++ frontend/src/pages/Cart/index.tsx
@@ -2,31 +2,38 @@ import EmptyPage from "@/components/Empty"
 import FavoriteCard from "@/components/FavoriteCard"
 import { Title } from "@/components/Titlte"
 import { useCart } from "@/hooks/useCart"
+import { ProductProps } from "@/types/ProductTypes"
 import { Stack, SimpleGrid } from "@mantine/core"
 
 export function Cart() {
     const userId = localStorage.getItem("user")
     const { cart } = useCart(userId)
+    const hasProductsInCart = cart && cart.length > 0
 
     return (
-        <main className="px-4 md:px-12 pb-8">
-            {cart && cart.length > 0 ? (
-                <Stack>
-                    <Title text="Cart" />
+        <Stack px={{ base: 30, md: 52 }} mb={6}>
+            <Title text="Cart" />
+            {hasProductsInCart ? CartProductsContainer(cart) : CartEmpty()}
+        </Stack>
+    )
+}
 
-                    <SimpleGrid cols={{ base: 2, sm: 3, md: 4, lg: 5 }}>
-                        {cart.map((product) => (
-                            <FavoriteCard key={product._id} product={product} />
-                        ))}
-                    </SimpleGrid>
-                </Stack>
-            ) : (
-                <EmptyPage
-                    message="No products in your cart."
-                    hasButton
-                    buttonText="Go shopping"
-                />
-            )}
-        </main>
+const CartProductsContainer = (cart: ProductProps[]) => {
+    return (
+        <SimpleGrid cols={{ base: 2, sm: 3, md: 4, lg: 5 }}>
+            {cart.map((product) => (
+                <FavoriteCard key={product._id} product={product} />
+            ))}
+        </SimpleGrid>
+    )
+}
+
+const CartEmpty = () => {
+    return (
+        <EmptyPage
+            message="No products in your cart."
+            hasButton
+            buttonText="Go shopping"
+        />
     )
 }
