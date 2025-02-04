c03c2996b6713c0eef3b7a1ee1edb5cd4fdd62d5
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-02-04 11:40:14 -0300

    feat: ajustar largura do componente Stack na página inicial para 100%

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -20,7 +20,7 @@ export function Home() {
             >
                 <Trends />
 
-                <Stack>
+                <Stack w="100%">
                     <PromoCard />
                     <Products />
                 </Stack>
