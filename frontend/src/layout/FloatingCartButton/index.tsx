04e844267ac23ccb7a462671331b2d6bcb7a0ec8
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-02-06 16:31:06 -0300

    feat: adicionar botão flutuante do carrinho e refatorar layout para utilizá-lo com animação ao adicionar item ao carrinho

---
+++ frontend/src/layout/FloatingCartButton/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { Affix, Indicator, ActionIcon } from "@mantine/core"
+import { BiCart } from "react-icons/bi"
+import { useCartContext } from "@/contexts/CartContext"
+
+export default function FloatingCartButton() {
+    const [isAnimating, setIsAnimating] = useState(false)
+    const { cartTotal } = useCartContext()
+
+    useEffect(() => {
+        if (cartTotal > 0) {
+            setIsAnimating(true)
+            setTimeout(() => setIsAnimating(false), 500)
+        }
+    }, [cartTotal])
+
+    return (
+        <Affix position={{ bottom: 30, right: 30 }}>
+            <AnimatePresence>
+                <motion.div
+                    key={cartTotal}
+                    initial={{ scale: 1 }}
+                    animate={{ scale: isAnimating ? 1.1 : 1 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 200,
+                        damping: 10,
+                    }}
+                >
+                    <Indicator
+                        color="pink"
+                        offset={5}
+                        label={cartTotal}
+                        size={20}
+                    >
+                        <ActionIcon
+                            component="a"
+                            href="/cart"
+                            color="pink"
+                            variant="outline"
+                            bg="white"
+                            radius="xl"
+                            size={60}
+                        >
+                            <BiCart size={30} />
+                        </ActionIcon>
+                    </Indicator>
+                </motion.div>
+            </AnimatePresence>
+        </Affix>
+    )
+}
--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -3,28 +3,15 @@ import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
 import { NavbarItems } from "./Navbar"
 import { Footer } from "./Footer"
-import { Affix, ActionIcon, Indicator } from "@mantine/core"
-import { BiCart } from "react-icons/bi"
-import { useAuth } from "@/contexts/AuthContext"
+import FloatingCartButton from "./FloatingCartButton"
+// Utils
 import isMobile from "@/utils/isMobile"
-import { AnimatePresence, motion } from "framer-motion"
-import { useCartContext } from "@/contexts/CartContext"
-import { useState, useEffect } from "react"
+// Contexts
+import { useAuth } from "@/contexts/AuthContext"
 
 export function Layout() {
     const mobile = isMobile()
     const { user } = useAuth()
-    const { cartTotal } = useCartContext()
-    const [isAnimating, setIsAnimating] = useState(false)
-
-    useEffect(() => {
-        console.log(cartTotal)
-
-        if (cartTotal > 0) {
-            setIsAnimating(true)
-            setTimeout(() => setIsAnimating(false), 500)
-        }
-    }, [cartTotal])
 
     return (
         <>
@@ -36,41 +23,7 @@ export function Layout() {
                 <Outlet />
             </main>
 
-            {user && mobile && (
-                <Affix position={{ bottom: 30, right: 30 }}>
-                    <AnimatePresence>
-                        <motion.div
-                            key={cartTotal}
-                            initial={{ scale: 1 }}
-                            animate={{ scale: isAnimating ? 1.3 : 1 }}
-                            transition={{
-                                type: "spring",
-                                stiffness: 200,
-                                damping: 10,
-                            }}
-                        >
-                            <Indicator
-                                color="pink"
-                                offset={5}
-                                label={cartTotal}
-                                size={20}
-                            >
-                                <ActionIcon
-                                    component="a"
-                                    href="/cart"
-                                    color="pink"
-                                    variant="outline"
-                                    bg="white"
-                                    radius="xl"
-                                    size={60}
-                                >
-                                    <BiCart size={30} />
-                                </ActionIcon>
-                            </Indicator>
-                        </motion.div>
-                    </AnimatePresence>
-                </Affix>
-            )}
+            {user && mobile && <FloatingCartButton />}
 
             <Footer />
         </>
