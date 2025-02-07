c3b37631724be4344cbc1c90b97f5ec9f2c47434
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-02-07 16:53:45 -0300

    feat: atualizar título da categoria de "Accessories" para "Winter" e remover categoria "Beach"

--- frontend/src/pages/Home/CategoriesBlock/index.tsx
+++ frontend/src/pages/Home/CategoriesBlock/index.tsx
@@ -14,14 +14,9 @@ const categories = [
     },
     {
         id: 3,
-        title: "Accessories",
+        title: "Winter",
         cover: "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?q=80&w=1773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
-    // {
-    //     id: 4,
-    //     title: "Beach",
-    //     cover: "https://plus.unsplash.com/premium_photo-1680411666376-780805551be8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fG1vZGElMjBwcmFpYXxlbnwwfDB8MHx8fDA%3D",
-    // },
 ]
 
 export function CategoriesCards() {
